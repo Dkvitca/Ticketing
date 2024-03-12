16c0530e50fed4c68758248c109f47fcaac45b98
Dkvitca <dankvitca326@gmail.com>
2024-03-12 23:23:44 +0200

    fixed errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,18 @@
-
 export class DatabaseConnectionError extends Error {
-  reason = "Error connecting to database";
-
-  constructor() {
-    super();
-
-    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    statusCode = 500;
+    reason = "Error connecting to database";
+  
+    constructor() {
+      super();
+  
+      Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+    serializeErrors() {
+      return [
+        {
+          message: this.reason,
+        },
+      ];
+    }
   }
-}
+  
