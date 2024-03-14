fd327bee42d468f9fd6889b5af21db79a19e7a0d
Dkvitca <dankvitca326@gmail.com>
2024-03-14 15:18:05 +0200

    more error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import 'express-async-errors';
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
@@ -21,7 +22,18 @@ app.all('*', () => {
 });
 app.use(errorHandler);
 
+const start = async () => {
+  try{
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('connected to mongodb');
+  } catch (err) {
+      console.log(err)
+  }
+  
+}
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!!!!!!");
 });
+
+start();
